452a3c47fd00d19048df3526691329badcaf4b45
Aditya-M-Shanbhag <adityamshanbhag24@gmail.com>
2024-11-25 18:18:36 +0530

    Update BCD_tb.sv

--- banana/BCD_tb.sv
+++ banana/BCD_tb.sv
@@ -7,7 +7,7 @@ eightbit I1 (a,b,cin,s,cout);
 initial
 begin
 a=8'b00000110;b=8'b00000011;cin=1'b0;
-#10 a=8'b00010101;b=8'b01011001;cin=1'b0;
-#10 a=8'b10001001;b=8'b00111001;cin=1'b1;
+#10 a=8'b00010101;b=8'b00011001;cin=1'b0;
+#10 a=8'b00011001;b=8'b00011001;cin=1'b1;
 end
 endmodule
